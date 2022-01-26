7e047fdd215b0506d7788aac48b1b57e579f9aef
EunYoung <pandamon24@naver.com>
2022-01-27 01:59:24 +0900

    index.html 위치 변경에 따라 webpack 설정 변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
       filename: "index.html",
     }),
     new CleanWebpackPlugin(),
