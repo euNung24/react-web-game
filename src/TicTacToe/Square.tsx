a03c67beda3216880b348400faf7ce13f08ac6ce
eunung <eunung@gmail.com>
2023-11-24 21:23:28 +0900

    feat: square의 value를 부모 컴포넌트에서 props로 넘겨주기

--- src/TicTacToe/Square.tsx
+++ src/TicTacToe/Square.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Square() {
-  const [value, setValue] = useState<string>("");
-  function handleClick() {
-    setValue("X");
-  }
-
+export default function Square({
+  value = null,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}) {
   return (
-    <button type="button" className="square" onClick={handleClick}>
+    <button type="button" className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
