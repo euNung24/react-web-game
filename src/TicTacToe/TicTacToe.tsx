69295f8932e7136990a4618d25b3039a9055a25a
eunung <eunung@gmail.com>
2023-11-25 22:23:38 +0900

    feat: xIsNext state에서 분리

--- src/TicTacToe/TicTacToe.tsx
+++ src/TicTacToe/TicTacToe.tsx
@@ -3,21 +3,19 @@ import Board from "./Board";
 import Info from "./Info";
 
 export default function TicTacToe() {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<string[][]>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState<number>(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquare = history[currentMove];
 
   function handlePlay(newSquares: string[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), newSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   return (
