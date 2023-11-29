ad47201c4c60960863cb6ce30c98c6b79574c0f1
eunung <eunung@gmail.com>
2023-11-29 18:21:43 +0900

    feat: TicTacToe게임 Home에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { HashRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Snake from "./Snake/components/Snake";
 import MineSearch from "./MineSearch/components/MineSearch";
+import TicTacToe from "./TicTacToe/TicTacToe";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/snake" component={Snake} />
-      <Route path="/mineSearch" component={MineSearch} />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/snake" element={<Snake />} />
+        <Route path="/mineSearch" element={<MineSearch />} />
+        <Route path="/tictactoe" element={<TicTacToe />} />
+      </Routes>
     </Router>
   );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 import mineSearch from "./HeadImage/mineSearch.png";
 import snake from "./HeadImage/snake.png";
 
@@ -9,47 +9,56 @@ const GlobalStyle = createGlobalStyle`
   body {
     background: #555;
   }
+
   a {
     color: #000;
     text-decoration: none;
     text-align: center;
   }
+
   a:hover {
     color: #cdcdcd;
   }
+
   ul {
     list-style: none;
     display: flex;
     margin: 0;
     padding: 0;
   }
+
   img {
     width: 400px;
     height: 300px;
     vertical-align: bottom;
   }
+
   .title {
     margin: 3rem;
     text-align: center;
     font-size: 2rem;
   }
+
   .game_lists {
     margin: 0 auto;
     display: flex;
     justify-content: space-around;
     flex-wrap: wrap;
   }
+
   .game_lists p {
     font-size: 20px;
     font-weight: bold;
   }
-`
+`;
 
 const Home = () => {
   return (
     <>
       <GlobalStyle />
-      <h2 className="title"><Link to="/">Web Games</Link></h2>
+      <h2 className="title">
+        <Link to="/">Web Games</Link>
+      </h2>
       <ul className="game_lists">
         <li>
           <Link to="/mineSearch">
@@ -61,6 +70,12 @@ const Home = () => {
           <Link to="/snake">
             <img src={snake} alt="뱀게임" />
             <p>뱀게임</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/tictactoe">
+            {/*<img src={tictactoe} alt="tictactoe" />*/}
+            <p>TicTacToe</p>
           </Link>
         </li>
       </ul>
